9378197ee07fe40b091f6147b18903c2e3700a43
xr686 <111225775+xr686@users.noreply.github.com>
2026-03-26 14:47:57 +0800

    Delete recamera-gimbal/scripts directory

--- recamera-gimbal/scripts/record_audio.ps1
+++
@@ -1,4 +0,0 @@
-$RecameraIp = "192.168.31.198"
-$RecameraPass = "recamera.1"
-
-ssh -o StrictHostKeyChecking=no recamera@$RecameraIp "echo '$RecameraPass' | sudo -S aplay -D hw:1,0 /home/recamera/test.wav"
